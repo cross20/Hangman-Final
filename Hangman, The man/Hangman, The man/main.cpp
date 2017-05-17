953b8be8b707e384b6d17d42ffc6c2936c7690dd
Chad Ross <rosschad98@icloud.com>
2017-05-17 12:17:49 -0700

    Clean Version
    Cleaned up the code (tim)

--- Hangman, The man/Hangman, The man/main.cpp
+++ Hangman, The man/Hangman, The man/main.cpp
@@ -1,76 +1,56 @@
- #include <iostream>
- #include <fstream>
- #include <string>
- #include <vector>
- 
- using namespace std;
- 
- bool isValidLetter(vector<char> Word, vector<char> displayWord, char guessedLetter, int& PENALTY, vector<char>& guessedLetters);
- void displayGame(vector<char> displayWord, vector<char> guessedLetters, int PENALTY);
-void hangman(int& PENALTY);
- 
- int main() {
- 
-	/////////////////////////////////////////////////
-	//Example of a word. This will either be randomly selected from the txt file or cin by a second player and then clear screened.
-	//I chose this case so we can see what happens with the apostrophe.
-	string word = "shouldn't";
- 
-	//Example to open file for word selection:
-	ifstream file;
-	file.open("words_no_numbers.txt");
-	if (!file.is_open()) cout << "NOPE";
-	/*while (file >> word)
-	{
-	cout << word << '\n';
-	}*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <random>
 
-////////////////////////////////////////////////
+using namespace std;
 
-//Change string into vector of chars and vector to be displayed
-vector<char> Word(word.begin(), word.end());
-vector<char> displayWord;
-for (int i = 0; i < Word.size(); i++) {
-    if (Word[i] == 39) {
-        displayWord.push_back(39);
-    }
-    else {
-        displayWord.push_back('_');
-    }
-}
-//
+void createWord(int randomNumber, string& word, vector<char>& displayWord);
+void createDisplayWord(vector<char> Word, vector<char>& displayWord);
+void initialScreen(vector<char> displayWord);
+bool isValidLetter(vector<char> Word, vector<char> displayWord, char guessedLetter, int& PENALTY, vector<char>& guessedLetters);
+void displayGame(vector<char> displayWord, vector<char> guessedLetters, int PENALTY);
+void hangman(int PENALTY);
+bool winLoseCheck(vector<char> displayWord, int PENALTY, string word);
 
-//Additional variables needed. The player's guessed letter, the list of all their guessed letters, and how many letters they have gotten incorrect
-char guessedLetter;
-vector<char> guessedLetters;
-int PENALTY = 0;
-//
-
-//Displaying initial screen
-cout << "Welcome to hangman...May the odds be ever in your favor." << "\n\n" << "START" << endl << endl;
-for (int i = 0; i < displayWord.size(); i++) {
-    cout << displayWord[i] << " ";
-}
-     
-     cout << endl;
-     
-//
-
-//Infinite loop to play the game
-while (true) {
+int main() {
+    srand(time(NULL));
+    int randomNumber = 0;
+    string word;
+    char guessedLetter;
+    vector<char> guessedLetters;
+    vector<char> displayWord;
+    int PENALTY = 0;
     
-    //Player guesses a letter
-    cout << endl << "What letter would you like to guess?" << endl << "> ";
-    cin >> guessedLetter;
+    //Choose a word randomly from the file and change string into vector of chars to be displayed
+    createWord(randomNumber, word, displayWord);
+    vector<char> Word(word.begin(), word.end());
+    createDisplayWord(Word, displayWord);
+    //
     
-    //Checks for validity of letter
-    if (isValidLetter(Word, displayWord, guessedLetter, PENALTY, guessedLetters)) {
+    //Displaying initial screen
+    initialScreen(displayWord);
+    //
+    
+    //Infinite loop to play the game
+    while (true) {
+        
+        //Player guesses a letter
+        cout << endl << "What letter would you like to guess?" << endl << "> ";
+        cin >> guessedLetter;
         //
         
-        //Searches for letter and changes it in the display word
-        for (int i = 0; i < displayWord.size(); i++) {
-            if (guessedLetter == Word[i]) {
-                displayWord[i] = Word[i];
+        //Checks for validity of letter
+        if (isValidLetter(Word, displayWord, guessedLetter, PENALTY, guessedLetters)) {
+            //
+            
+            //Searches for letter and changes it in the display word
+            for (int i = 0; i < displayWord.size(); i++) {
+                if (guessedLetter == Word[i]) {
+                    displayWord[i] = Word[i];
+                }
             }
         }
         //
@@ -79,28 +59,57 @@ while (true) {
         displayGame(displayWord, guessedLetters, PENALTY);
         //
         
-        //Checks if the player has won the game
-        bool hasWon = true;
-        for (int i = 0; i < displayWord.size(); i++) {
-            if (displayWord[i] == '_')
-                hasWon = false;
-        }
-        if (hasWon == true) {
-            cout << "Yay! You won!" << endl;
+        //Conditions to end the game
+        if (winLoseCheck(displayWord, PENALTY, word)) {
+            bool stop; cin >> stop;
             return 0;
         }
         //
     }
-    else {
-        //Displays the word up to this point
-        displayGame(displayWord, guessedLetters, PENALTY);
-        //
+    //
+}
+
+//Choose a word randomly from the file and change string into vector of chars to be displayed
+void createWord(int randomNumber, string& word, vector<char>& displayWord) {
+    random_device rd;
+    std::default_random_engine generator(rd());
+    std::uniform_int_distribution<long long unsigned> distribution(0, 354910);
+    ifstream file;
+    file.open("words_no_numbers.txt");
+    if (!file.is_open()) cout << "Essential word file not found.";
+    randomNumber = distribution(generator);
+    for (int i = 0; i < randomNumber; i++) {
+        file >> word;
     }
-    
-    
 }
 //
+
+//Change string into vector of chars to be displayed
+void createDisplayWord(vector<char> Word, vector<char>& displayWord) {
+    for (int i = 0; i < Word.size(); i++) {
+        if (Word[i] == 39) {
+            displayWord.push_back(39);
+        }
+        else {
+            displayWord.push_back('_');
+        }
+    }
 }
+//
+
+//Displaying initial screen
+void initialScreen(vector<char> displayWord) {
+    cout << "Welcome to hangman...  May the odds be ever in your favor." << endl << endl << endl;
+    for (int i = 0; i < displayWord.size(); i++) {
+        cout << displayWord[i] << " ";
+    }
+    cout << endl << "Letters you have guessed:" << endl << endl;
+    hangman(0);
+    cout << endl;
+    
+    cout << endl << "PENALTY: 0" << endl;
+}
+//
 
 //Logic to determine if that guess is valid, if it has already been guessed, or if it is part of the word
 bool isValidLetter(vector<char> Word, vector<char> displayWord, char guessedLetter, int& PENALTY, vector<char>& guessedLetters) {
@@ -144,66 +153,27 @@ bool isValidLetter(vector<char> Word, vector<char> displayWord, char guessedLett
 }
 //
 
-void hangman(int& PENALTY)
+//Display the hangman
+void hangman(int PENALTY)
 {
-    string head = "|   O", body = "|   |", rightArmLeg = "|   |_", bothArmsLegs = "|  _|_", top = "_____", emptyRope = "|   |", emptyWood = "|";
-    
-    if ( PENALTY == 0 )
-    {
-        cout << top << endl << emptyRope << endl;
-        
-        for ( int i = 0; i < 5; i++ )
-        {
-            cout << emptyWood << endl;
-        }
-    }
-    
-    else if ( PENALTY == 1 || PENALTY == 2)
-    {
-        cout << top << endl << emptyRope << endl << head << endl;
-        if ( PENALTY == 2 )
-        {
-            cout << body << endl << emptyWood << endl << emptyWood << endl << emptyWood << endl << endl;
-        }
-        else
-        {
-            cout << emptyWood << endl << emptyWood << endl << emptyWood << endl << emptyWood << endl;
-        }
-    }
-    
-    else if ( PENALTY == 3 )
-    {
-        cout << top << endl << emptyRope << endl << head << endl << rightArmLeg << endl << emptyWood << endl << emptyWood << endl << emptyWood << endl;
-    }
-    
-    else if ( PENALTY == 4 )
-    {
-        cout << top << endl << emptyRope << endl << head << endl << bothArmsLegs << endl << emptyWood << endl << emptyWood << endl << emptyWood << endl;
-    }
-    
-    else if ( PENALTY == 5 )
-    {
-        cout << top << endl << emptyRope << endl << head << endl << bothArmsLegs << endl << body << endl << emptyWood << endl << emptyWood << endl;
-    }
-    
-    else if ( PENALTY == 6 )
-    {
-        cout << top << endl << emptyRope << endl << head << endl << bothArmsLegs << endl << body << endl << body << endl << emptyWood << endl;
-    }
-    
-    else if ( PENALTY == 7 )
-    {
-        cout << top << endl << emptyRope << endl << head << endl << bothArmsLegs << endl << body << endl << rightArmLeg << endl << emptyWood << endl;
-    }
-    
-    else if ( PENALTY >= 8 )
-    {
-        cout << top << endl << emptyRope << endl << head << endl << bothArmsLegs << endl << body << endl << bothArmsLegs << endl << emptyWood << endl;
-    }
+    string head = "|   O\n", body = "|   |\n", rightArmLeg = "|   |_\n", bothArmsLegs = "|  _|_\n", top = "_____\n", emptyRope = "|   |\n", emptyWood = "|\n";
+    cout << top << emptyRope;
+    if (PENALTY == 0) cout << emptyWood << emptyWood << emptyWood << emptyWood;
+    else if (PENALTY == 1) cout << head << emptyWood << emptyWood << emptyWood;
+    else if (PENALTY == 2) cout << head << body << emptyWood << emptyWood;
+    else if (PENALTY == 3) cout << head << rightArmLeg << emptyWood << emptyWood;
+    else if (PENALTY == 4) cout << head << bothArmsLegs << emptyWood << emptyWood;
+    else if (PENALTY == 5) cout << head << bothArmsLegs << body << emptyWood;
+    else if (PENALTY == 6) cout << head << bothArmsLegs << body << body;
+    else if (PENALTY == 7) cout << head << bothArmsLegs << body << rightArmLeg;
+    else cout << head << bothArmsLegs << body << bothArmsLegs;
+    cout << emptyWood;
 }
+//
 
-//Displays the hangman(not yet done), their word so far, and their guessed letters
+//Displays the hangman, their word so far, their guessed letters, and their PENALTY number
 void displayGame(vector<char> displayWord, vector<char> guessedLetters, int PENALTY) {
+    
     //Displays the word
     for (int i = 0; i < displayWord.size(); i++) {
         cout << displayWord[i] << " ";
@@ -212,21 +182,42 @@ void displayGame(vector<char> displayWord, vector<char> guessedLetters, int PENA
     
     //Displays the guessed letters
     cout << endl << "Letters you have guessed: ";
-        
     for (int i = 0; i < guessedLetters.size(); i++) {
         cout << guessedLetters[i] << ", ";
     }
-    
     cout << endl << endl;
+    //	
     
+    //Display hangman
     hangman(PENALTY);
-    
     cout << endl;
     //
+    
+    cout << endl << "PENALTY: " << PENALTY << endl;
 }
+//
 
-
-
-
-
-
+//Checks if you have won or lost the game. If so, the game is over
+bool winLoseCheck(vector<char> displayWord, int PENALTY, string word) {
+    
+    //Checks if the player has won the game
+    bool hasWon = true;
+    for (int i = 0; i < displayWord.size(); i++) {
+        if (displayWord[i] == '_')
+            hasWon = false;
+    }
+    if (hasWon == true) {
+        cout << "Yay! You won!" << endl;
+        return true;
+    }
+    //
+    
+    //Checks if the player has lost the game
+    else if (PENALTY > 7) {
+        cout << "Boo, you lost. The word was: " << word << endl;
+        return true;
+    }
+    //
+    
+    return false;
+}
